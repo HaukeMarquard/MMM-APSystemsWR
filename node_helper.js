3dd7eed56f4b1534c6e5331272fa4d186ec25eb8
HaukeMarquard <120113101+HaukeMarquard@users.noreply.github.com>
2023-11-30 15:36:44 +0100

    correct url

--- node_helper.js
+++ node_helper.js
@@ -11,7 +11,7 @@ module.exports = NodeHelper.create({
   getWeather: function (payload) {
     var that = this;
     // this.url = `https://api.tomorrow.io/v4/weather/forecast?location=${payload.lat},${payload.lon}&apikey=${payload.api_key}`
-    this.url = `http://192.168.178.62:8050/getOutputData`;
+    this.url = `http://192.168.178.61:8050/getOutputData`;
     Log.info(this.url);
     axios.get(this.url).then((response) => {
       that.sendSocketNotification("WR_RESULT", response.data);
